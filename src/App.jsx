0dfe9656b4461568961ce23a067cd5668114b9e3
Guilherme Miyamoto Bragatto <guimbragatto@exemplo.com>
2025-10-27 19:55:49 -0300

    Primeiro commit - projeto iPhone

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import NavBar from "./components/NavBar";
+import Hero from "./components/hero";
+import Intro from "./components/intro";
+import Highlights from "./components/highlights";
+import Colors from "./components/colors";
+import Footer from "./components/footer";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Hero />
+      <Intro />
+      <Highlights />
+      <Colors />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
